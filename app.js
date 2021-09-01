b999d0e002451497468c2502cae2626493450297
Janouy <jeanne.boutry@hotmail.fr>
2021-09-01 12:44:05 +0200

    test routes ok

--- app.js
+++ app.js
@@ -18,6 +18,38 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get('/api/sauces', (req, res, next) => {
+  const sauce = [
+    {
+    _id: 'bvhfjde',
+    name: 'Sauce piquante 1',
+    manufacturer: 'Steeve',
+    description: 'hot hot hot',
+    mainPepper: 'piment de la mort',
+    imageUrl: 'https://images.pexels.com/photos/4874483/pexels-photo-4874483.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    heat: 10,
+    likes: 321,
+    dislikes: 42,
+    usersLiked: [],
+    usersDisliked: [],
+    },
+    {
+    _id: 'bvhfbhg',
+    name: 'Sauce piquante 2',
+    manufacturer: 'Miranda',
+    description: 'so hot',
+    mainPepper: 'piment muy forte',
+    imageUrl: 'https://images.pexels.com/photos/5738091/pexels-photo-5738091.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    heat: 7,
+    likes: 452,
+    dislikes: 36,
+    usersLiked: [],
+    usersDisliked: [],
+    },
+  ];
+  res.status(200).json(sauce);
+}),
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
