59602aec690fe3c2e797bb0f080915c872c87c9c
Janouy <jeanne.boutry@hotmail.fr>
2021-09-16 10:26:18 +0200

    Ajout longueur mot de passe

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,9 @@ const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
 const createImages = require('../middleware/createImages');
+const passwordLength = require('../middleware/password');
 
-router.post('/signup', createImages, userCtrl.signup);
+router.post('/signup', passwordLength, createImages, userCtrl.signup);
 router.post('/login', createImages, userCtrl.login);
 
 module.exports = router;
